934a69813ebd06c3d6cca8320ea8a02eccf60d43
Demitri Morgan <demitri@pagerduty.com>
2025-02-14 16:21:27 -0700

    Fix sphinx build reference to module version

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -7,7 +7,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(
     os.path.dirname(__file__),
     '..', '..'
 )))
-from setup import __version__
+from pagerduty import __version__
 import pagerduty
 
 # -- Project information -----------------------------------------------------
