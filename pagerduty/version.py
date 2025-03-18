b3bdbe69f90831b7e137817e34ccdcd86222880b
Demitri Morgan <demitri@pagerduty.com>
2025-03-18 17:47:14 -0600

    Use backwards-compatible-with-3.6-3.7 version fetching

--- pagerduty/version.py
+++ pagerduty/version.py
@@ -1,3 +1,14 @@
-from importlib.metadata import version
+import sys
 
-__version__ = version(__package__)
+def derived_version():
+    if sys.version_info.major == 3 and sys.version_info.minor < 8:
+        # There is no way to derive the version from the package metadata because the
+        # necessary importlib features have not yet been added. At some point we need to
+        # drop support for these versions so that we can stop updating this version
+        # manually in two places and set the version in only one place, pyproject.toml
+        return "2.0.0"
+    else:
+        from importlib.metadata import version
+        return version(__package__)
+
+__version__ = derived_version()
