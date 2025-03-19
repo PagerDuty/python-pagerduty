6757116d1814eb0f2e73f5cec8aa41c3e58889b6
Demitri Morgan <demitri@pagerduty.com>
2025-03-18 18:02:33 -0600

    Use a fake version for Python versions prior to 3.8 because I am done with manually updating the version in 2+ places

--- pagerduty/version.py
+++ pagerduty/version.py
@@ -4,9 +4,8 @@ def derived_version():
     if sys.version_info.major == 3 and sys.version_info.minor < 8:
         # There is no way to derive the version from the package metadata because the
         # necessary importlib features have not yet been added. At some point we need to
-        # drop support for these versions so that we can stop updating this version
-        # manually in two places and set the version in only one place, pyproject.toml
-        return "2.0.0"
+        # drop support for these versions.
+        return "2.x.x"
     else:
         from importlib.metadata import version
         return version(__package__)
