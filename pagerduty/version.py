eac1ebd7efe7e743ee44c8f6c5875a5b3f3a7c03
Demitri Morgan <demitri@pagerduty.com>
2025-03-19 15:38:02 -0600

    Trivial name/comment changes in version.py

--- pagerduty/version.py
+++ pagerduty/version.py
@@ -1,13 +1,14 @@
 import sys
 
-def derived_version():
+def get_version():
     if sys.version_info.major == 3 and sys.version_info.minor < 8:
-        # There is no way to derive the version from the package metadata because the
+        # There is no way to obtain the version from the package metadata because the
         # necessary importlib features have not yet been added. At some point we need to
-        # drop support for these versions.
+        # drop support for these versions. We only care about major version 3 because
+        # version 2 is already not supported.
         return "2.x.x"
     else:
         from importlib.metadata import version
         return version(__package__)
 
-__version__ = derived_version()
+__version__ = get_version()
