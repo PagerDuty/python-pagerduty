051bed1f2d1f77babd270eb2094e163fed06b6a4
Demitri Morgan <demitri@pagerduty.com>
2025-07-16 13:39:45 -0600

    Change version test to run only in >3.11 (it only needs to run in one Python version)

--- tests/version_test.py
+++ tests/version_test.py
@@ -1,3 +1,4 @@
+import sys
 import unittest
 
 import pagerduty
@@ -6,18 +7,21 @@ class VersionTest(unittest.TestCase):
 
     def test_major_version(self):
         """
-        Validate that the major "unknown version" matches the same as the actual
+        Validate that the major component of "unknown version" matches the actual
 
-        This ensures we have something to track major versions with.
+        This ensures we have something to track major versions with, in Python versions
+        where package introspection isn't available.
 
-        It is tautological in versions less than 11, but we only need to see it fail in
-        one version to know that it needs to be updated. This is because the
-        get_version() method uses tomllib to extract the version from pyproject.toml
-        i.e. when running in a unit test, because the package may not have been built
-        yet depending on the test environment (and this is required for using importlib)
+        The test is tautological and won't even run in versions less than 11
+        (ImportError) because tomllib was added in version 3.11. However, we only need to see it fail in one version to know that it
+        needs to be updated. This is because the get_version() method uses tomllib
+        (added in 3.11) to extract the version from pyproject.toml i.e. when running in
+        a unit test, because the package may not have been built yet depending on the
+        test environment (and this is required for using importlib)
         """
-        ver_in_toml = pagerduty.version.get_version_via_tomllib()
-        ver_unknown = pagerduty.version.UNKNOWN_VERSION
-        ver_from_fn = pagerduty.version.get_version()
-        self.assertEqual(*[v.split('.')[0] for v in (ver_in_toml, ver_unknown)])
-        self.assertEqual(*[v.split('.')[0] for v in (ver_unknown, ver_from_fn)])
+        if sys.version_info.major == 3 and sys.version_info.minor > 10:
+            ver_in_toml = pagerduty.version.get_version_via_tomllib()
+            ver_unknown = pagerduty.version.UNKNOWN_VERSION
+            ver_from_fn = pagerduty.version.get_version()
+            self.assertEqual(*[v.split('.')[0] for v in (ver_in_toml, ver_unknown)])
+            self.assertEqual(*[v.split('.')[0] for v in (ver_unknown, ver_from_fn)])
