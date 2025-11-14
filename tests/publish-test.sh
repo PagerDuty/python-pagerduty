64c173f3f78cd4c389a0094b032614e28dc2b216
Demitri Morgan <demitri@pagerduty.com>
2025-11-14 11:44:59 -0700

    fix(71): use uv in testpublish

--- tests/publish-test.sh
+++ tests/publish-test.sh
@@ -1,27 +1,56 @@
 #!/bin/bash
-
 set -e
 
+PACKAGE='pagerduty'
+VERSION=`grep '^version' pyproject.toml | cut -d'"' -f2 | head -n 1`
+
 refresh_virtualenv () {
-    echo "Re-creating temporary virtualenv"
+    echo "--- Re-creating temporary virtualenv"
     rm -rf ./tmp
     mkdir -p ./tmp
-    virtualenv ./tmp >/dev/null 2>&1
+    uv run virtualenv ./tmp >/dev/null 2>&1
 }
 
-echo "Uploading to test.pypi.org"
-twine upload --repository testpypi dist/*
+py_test () {
+  echo "--- Using Python: `which python`"
+  python - <<EOF
+import sys
+if '${PACKAGE}' in sys.modules:
+  del sys.modules['${PACKAGE}']
+import ${PACKAGE}
+print(${PACKAGE}.__version__)
+print(${PACKAGE}.__file__)
+EOF
+  [[ $? == 0 ]] && echo "--- Success!" || echo "--- Failure."
+}
 
+echo "--- Testing publish / test install ${PACKAGE}==${VERSION}"
+
+if [[ "${SKIP_UPLOAD:-0}" == "0" ]]; then
+  echo "--- Uploading to test.pypi.org"
+  uv publish --indexpublish-url 'https://test.pypi.org/legacy/' --username __token__
+fi
+
+echo "--- Testing install from scratch"
 refresh_virtualenv
-source ./tmp/bin/activate
-echo "Testing install from scratch"
-pip install --index-url https://test.pypi.org/simple/ pagerduty
+pushd ./tmp
+source ./bin/activate
+echo "--- Using pip: `which pip`"
+# This hack is necessary because httpx is not present in the test index:
+pip install -r ../requirements.txt 
+pip install --index-url https://test.pypi.org/simple/ "${PACKAGE}==${VERSION}"
+py_test
 deactivate
+popd
 
+echo "--- Testing install of mainline version and then upgrade from testing index"
 refresh_virtualenv
-source ./tmp/bin/activate
-echo "Testing install and then upgrade"
+pushd ./tmp
+source ./bin/activate
+echo "--- Using pip: `which pip`"
 pip install pagerduty
-pip install --upgrade --index-url https://test.pypi.org/simple/ pagerduty
+pip install --upgrade --index-url https://test.pypi.org/simple/ "${PACKAGE}==${VERSION}"
+py_test
 deactivate
+popd
 rm -rf tmp
