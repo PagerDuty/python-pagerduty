1dc5a2a13b4519ea18d442562622c0ec4250cf30
Demitri Morgan <demitri@pagerduty.com>
2026-01-08 11:21:44 -0700

    fix(61): tests for AuthMethod

--- tests/auth_method_test.py
+++ tests/auth_method_test.py
@@ -1,5 +1,55 @@
 import unittest
 
+from pagerduty import auth_method
 
-class AuthMethodTest(unittest.TestCase):
-    pass
+class AuthMethodBaseTest(unittest.TestCase):
+
+    @property
+    def auth_method_class(self):
+        return auth_method.AuthMethod
+
+    def new_auth_method(self):
+        return self.auth_method_class("token")
+
+
+def AuthMethodTest(AuthMethodBaseTest):
+
+    def test_auth_header(self):
+        am = self.new_auth_method()
+        with self.assertRaises(NotImplementedError):
+            ah = am.auth_header
+
+    def test_auth_param(self):
+        am = self.new_auth_method()
+        with self.assertRaises(NotImplementedError):
+            ap = am.auth_param
+
+class HeaderAuthMethodTest(AuthMethodBaseTest):
+
+    @property
+    def auth_method_class(self):
+        return auth_method.HeaderAuthMethod
+
+    def test_auth_param(self):
+        am = self.new_auth_method()
+        self.assertEqual({}, am.auth_param)
+
+class BodyParameterAuthMethodTest(AuthMethodBaseTest):
+
+    @property
+    def auth_method_class(self):
+        return auth_method.BodyParameterAuthMethod
+
+    def test_auth_header(self):
+        am = self.new_auth_method()
+        self.assertEqual({}, am.auth_header)
+
+class PassThruHeaderAuthMethodTest(AuthMethodBaseTest):
+
+    @property
+    def auth_method_class(self):
+        return auth_method.PassThruHeaderAuthMethod
+
+    def test_auth_header(self):
+        am = self.new_auth_method()
+        self.assertEqual({"Authorization": "token"}, am.auth_header)
